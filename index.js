d066e98979b2234f1ddbdf374b03a62d6f3d97da
blkis169 <qees169@gmail.com>
2025-05-27 13:35:48 +0300

    timer

---
+++ index.js
@@ -0,0 +1,14 @@
+let start_button = document.querySelector(".start");
+let timer_div = document.querySelector(".timer");
+
+
+
+
+start_button.addEventListener('click',)
+async function timermover(){
+
+
+}
+
+
+
