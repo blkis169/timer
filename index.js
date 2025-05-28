a5eb04b337741494cd9b87423576d5bc45bd574f
mariam2455 <mariam4vm@gmail.com>
2025-05-28 15:45:54 +0300

    chang in css and js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let start_button = document.querySelector(".start");
 let timer_div = document.querySelector(".timer");
+let h_m_s = document.querySelectorAll(".timer-border div ")
+let array_hms = Array.from(h_m_s);
+
+console.log(array_hms)
 
 
 
-
-start_button.addEventListener('click',)
-async function timermover(){
-
-
-}
 
 
 
